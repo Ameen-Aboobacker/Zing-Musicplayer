efac3d70516cbf1254847668d1100e94078d87ea
Ameen-Aboobacker <ameenkaboobacker95@gmail.com>
2023-03-01 18:41:18 +0530

    update drawer

--- lib/screens/drawer/drawer_about_us.dart
+++ lib/screens/drawer/drawer_about_us.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:music_app/screens/drawer/string.dart';
 
 class AboutUsDrawer extends StatelessWidget {
   const AboutUsDrawer({super.key});
@@ -7,41 +7,24 @@ class AboutUsDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container( 
+      body: Container(
         decoration: const BoxDecoration(
-      gradient: LinearGradient(
-        begin: Alignment.topCenter,
-        end: Alignment.bottomCenter,
-        colors: [
-          Color.fromARGB(255, 0, 0, 0),
-          Color.fromARGB(255, 0, 0, 0),
-        ],
-      ),
+          image: DecorationImage(
+              image: AssetImage('assets/images/bg.jpg'), fit: BoxFit.cover),
         ),
         child: Padding(
-      padding: const EdgeInsets.all(80.0),
-      child: Column(
-        children: const [
-         
-          Text(
-            """
-Welcome to Paattu Player Endless Music, 
-
-your number one source for all things Music. We're dedicated to providing you the best of Music, with a focus on dependability. customer service,.
-
-We're working to turn our passion for music into a booming Music Player. We hope you enjoy our Music as much as we enjoy offering them to you.
-
-Sincerely,
-
-Athul A V""",
-            style: TextStyle(
-                                 fontFamily: 'UbuntuCondensed',
-                                 color: Color.fromARGB(255, 15, 159, 167),
-                                fontSize: 19,   
-                              ), 
-          )
-        ],
-      ),
+          padding: const EdgeInsets.all(80.0),
+          child: Column(
+            children: const [
+              Text( about,
+                style: TextStyle(
+                  fontFamily: 'UbuntuCondensed',
+                  color: Color.fromARGB(255, 15, 159, 167),
+                  fontSize: 19,
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
